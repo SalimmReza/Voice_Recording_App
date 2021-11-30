26bb4d6d8c406f0a3a5639a7646a1a898bde2f0d
SalimmReza <refath300@gmail.com>
2021-12-01 00:20:22 +0600

    main design

---
+++ app/src/main/java/com/example/voicerecordingapp/Audio_list_F.java
@@ -0,0 +1,27 @@
+package com.example.voicerecordingapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Audio_list_F extends Fragment {
+
+
+
+    public Audio_list_F() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_audio_list_, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/voicerecordingapp/Record_Fragment.java
@@ -0,0 +1,24 @@
+package com.example.voicerecordingapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Record_Fragment extends Fragment {
+
+    public Record_Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_record_, container, false);
+    }
+}
